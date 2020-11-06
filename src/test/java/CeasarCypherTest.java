f5b5a4a82a60236b8c2c5fd48a6cd8155e419f74
Norbert Daniluk <norbert.daniluk@gmail.com>
2020-11-06 01:26:54 +0100

    Added tests for CeasarCypher class

---
+++ src/test/java/CeasarCypherTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CeasarCypherTest {
+
+    @Test
+    void encryptWithCorrectInterval() {
+        String interval = "21";
+        InputStream in = new ByteArrayInputStream(interval.getBytes());
+        System.setIn(in);
+
+        CeasarCypher ceasarCypher = new CeasarCypher();
+        String encryptedInput = ceasarCypher.encrypt("Norbert");
+        assertEquals("Ijmwzmo", encryptedInput);
+    }
+
+    @Test
+    void encryptWithIncorrectIntervalAndThrowException() {
+        String interval = "ObviouslyIncorrectInteger";
+        InputStream in = new ByteArrayInputStream(interval.getBytes());
+        System.setIn(in);
+
+        CeasarCypher ceasarCypher = new CeasarCypher();
+        assertThrows(InputMismatchException.class, () -> ceasarCypher.encrypt("Norbert"));
+    }
+
+    @Test
+    void encryptWithNotSupportedCharactersInInput(){
+        String interval = "3";
+        InputStream in = new ByteArrayInputStream(interval.getBytes());
+        System.setIn(in);
+
+        CeasarCypher ceasarCypher = new CeasarCypher();
+        String encryptedInput = ceasarCypher.encrypt("1337");
+        assertNull(encryptedInput);
+    }
+
+
+}
